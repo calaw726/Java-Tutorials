34c1c8223b82bc895f40f44141843004ef0d3fc1
calaw726 <89656489+calaw726@users.noreply.github.com>
2021-09-27 21:30:36 -0400

    Add files via upload
    Enhanced the Mortgage Calculator by limiting what the user can input to only reasonable values.

--- Mosh's Java Tutorial/Mortgage Calculator/src/MortgageCalculator.java
+++ Mosh's Java Tutorial/Mortgage Calculator/src/MortgageCalculator.java
@@ -4,18 +4,45 @@ import java.util.Scanner;
 public class MortgageCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
+
+        System.out.print("Principal ($1k - $1M): ");
         float principal = scanner.nextFloat();
+        while (!(principal >= 1000) || !(principal <= 1_000_000)) {
+            System.out.println("Enter a value between $1,000 and $1,000,000.");
+            System.out.print("Principal ($1k - $1M): ");
+            principal = scanner.nextFloat();
+        }
+
+
         scanner.nextLine();
+
         System.out.print("Monthly Interest Rate: ");
-        float r = scanner.nextFloat() / 100 / 12;
+        float r = scanner.nextFloat() ;
+        while (!(r > 0) || !(r <= 30)) {
+            System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            System.out.print("Monthly Interest Rate: ");
+            r = scanner.nextFloat();
+        }
+
+        r = r / 100 / 12;
         scanner.nextLine();
+
         System.out.print("Period (years): ");
-        float n = scanner.nextFloat() * 12;
+        float n = scanner.nextFloat();
+        while (!(n >= 1) || !(n <= 30)){
+            System.out.println("Enter a value between 1 and 30.");
+            System.out.print("Period (years): ");
+            n = scanner.nextFloat();
+
+        }
+
+
+        n = n * 12;
+
 
         float numerator = (float) (r * Math.pow((1 + r), n));
-        float denomenator = (float) ((Math.pow((1 + r), n)) - 1 );
-        float monthlyPayment = principal * (numerator / denomenator);
+        float denominator = (float) ((Math.pow((1 + r), n)) - 1 );
+        float monthlyPayment = principal * (numerator / denominator);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(monthlyPayment);
 
